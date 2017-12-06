79dfd11f5680bad7aa64df61bcb356ffaacda744
Manbec <manuel_bm@outlook.com>
2017-12-06 03:45:08 -0600

    DB and Readme Update

---
+++ MoviesDB.sql
@@ -0,0 +1,64 @@
+-- MySQL dump 10.13  Distrib 5.7.17, for macos10.12 (x86_64)
+--
+-- Host: localhost    Database: manbm_moviepost
+-- ------------------------------------------------------
+-- Server version	5.6.34
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `user_movies`
+--
+
+DROP TABLE IF EXISTS `user_movies`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user_movies` (
+  `iduser` varchar(255) NOT NULL,
+  `idmovie` int(11) NOT NULL,
+  `title` varchar(100) NOT NULL,
+  `overview` varchar(1000) NOT NULL,
+  `poster_path` varchar(100) NOT NULL,
+  `saved_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`iduser`,`idmovie`),
+  CONSTRAINT `usermovies_user_fk` FOREIGN KEY (`iduser`) REFERENCES `users` (`email`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `users` (
+  `email` varchar(255) NOT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `password` varchar(255) NOT NULL,
+  `remember_token` varchar(100) DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2017-12-06  3:24:40
--- Readme.md
+++ Readme.md
@@ -3,6 +3,23 @@
 
   The quickest way to get the latest information on movies you will like.
   
+  # How to use
+  
+- Import MoviesDB.sql to a mysql schema
+- check the following fields at laraverl->.env file
+
+    DB_HOST=localhost
+    DB_PORT=8889 (Change to your mysql port)
+    DB_DATABASE=manbm_moviepost (Change to your database)
+    DB_USERNAME=manbm (Change to your database user name)
+    DB_PASSWORD=moviepost123 (Change to your database user name's password)
+        -- Check user is added to DB_DATABASE database
+        
+- Set up a host (Ex. api.moviepost.loc) in your local hosts file, and set up a virtual host with servername *api.moviepost.loc* pointing to **laravel/public**  in order to be able to listen to the api.
+
+- The project https://github.com/Manbec/MoviePostFront has configured the api url as *api.moviepost.loc* and All requests are sent to that address. you can change it to localhost:port or any address you set for this API on your hosts file server setup.
+  
+  ## Other Notes
 ```huheuehue
 $ Application key [base64:UG0QwYwwFk+7b2EMl33F2LBZtP26x8qhm+sCncQUfek=] set successfully.
 ```
